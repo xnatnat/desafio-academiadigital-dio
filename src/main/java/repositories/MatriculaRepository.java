501630771d13f3e184ad00faad89b33f0ba55532
xnatnat <mota.nathalia963@gmail.com>
2023-04-11 19:23:01 -0300

    initial commit

---
+++ src/main/java/repositories/MatriculaRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import models.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+}
---
+++ src/test/java/desafio/spring/dio/AcademiaDigital/AcademiaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.spring.dio.AcademiaDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
